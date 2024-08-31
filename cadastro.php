b86fe0c6ee8262614ac50b670b588e1af65cd7c3
4lexandre4raujo <alexandresaraujo19@gmail.com>
2024-08-31 11:29:35 -0300

    Criação do arquivo conexao.php e atualizaçao de códigos.

---
+++ cadastro.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="style.css"></link>
+</head>
+<body>
+    <div class="main">
+     <div class="container_back">
+        <form action="" method="post">
+            <div class="container_form">
+            <div class="container_img">
+                <img src="img/logo.png" height="85" width="90">
+            </div>
+                <h3 align="center">Cadastre-se</h3>
+                Nome completo:
+                <br>
+                <input type="email" id="text">
+                <br>
+                Email:
+                <br>
+                <input type="email" id="email">
+                <br>
+                Telefone:
+                <br>
+                <input type="email" id="text">
+                <br>
+                Senha:
+                <br>
+                <input type="password" id="senha">
+                <br>
+                Confirmar Senha:
+                <br>
+                <input type="password" id="senha">
+                <br>
+                <label size="8px">Já possui conta? <a href="">Entrar</a></label>
+            </div>
+                <input type="submit" name="login" id="submit" value="Próximo" align="center">
+        </form>
+     </div>
+    </div>
+</body>
+</html>
--- cadastroEndereco.php
+++ cadastroEndereco.php
@@ -3,41 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Entrar</title>
+    <title>Cadastro Endereço</title>
     <link rel="stylesheet" href="style.css"></link>
 </head>
 <body>
     <div class="main">
-     <div class="container_backcad">
+     <div class="container_back">
         <form action="" method="post">
             <div class="container_form">
             <div class="container_img">
                 <img src="img/logo.png" height="85" width="90">
             </div>
                 <h3 align="center">Cadastre-se</h3>
-                Nome completo:
+                Endereço:
                 <br>
                 <input type="email" id="text">
                 <br>
-                Email:
+                Bairro:
                 <br>
                 <input type="email" id="email">
                 <br>
-                Telefone:
+                Cidade:
                 <br>
                 <input type="email" id="text">
                 <br>
-                Senha:
+                Número:
                 <br>
                 <input type="password" id="senha">
                 <br>
-                Confirmar Senha:
+                Complemento:
                 <br>
                 <input type="password" id="senha">
                 <br>
                 <label size="8px">Já possui conta? <a href="">Entrar</a></label>
             </div>
-                <input type="submit" name="login" id="submit" value="Entrar" align="center">
+                <input type="submit" name="login" id="submit" value="Finalizar cadastro" align="center">
         </form>
      </div>
     </div>
